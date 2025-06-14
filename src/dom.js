132eeee333b1e79da6c68c5fcd149b73f500ca2d
eQuinoxTR <tiziano313rivadeneira@gmail.com>
2025-06-14 11:53:09 -0300

    added icons and icon click functions

--- src/dom.js
+++ src/dom.js
@@ -4,9 +4,28 @@ export let pages = document.querySelectorAll(".container-of-notes");
 export function getCardDom(page) {
     const note = document.createElement("div");
     note.classList.add("note");
+
+    const headerTxt = document.createElement("div");
+    headerTxt.classList.add("header-txt")
+
+    const saveSvg = document.createElement("div");
+    saveSvg.id = "doneIcon";
+
+    const optionsSvg = document.createElement("div");
+    optionsSvg.id = "options";
+
     const input = document.createElement("input");
     input.placeholder = "Type a new task...";
-    note.appendChild(input);
+
+    note.appendChild(saveSvg);
+    note.appendChild(optionsSvg);
+    headerTxt.appendChild(input);
+    note.appendChild(headerTxt);
+
     const btn = page.querySelector(".new");
     return {note, input, btn}
+} 
+
+export function addPopOver(page) {
+    console.log("adding")
 } 
--- src/index.js
+++ src/index.js
@@ -1,92 +1,102 @@
 import "./styles.css";
-import {root, pages, getCardDom} from "./dom.js";
-
+import {root, pages, getCardDom, addPopOver} from "./dom.js";
+let theNoteIsSaved = false;
+let savedNote;
 (function() {
-    let theNoteIsSaved = false;
-    let savedNote;
-
     pages.forEach((page) => page.addEventListener("click", (e) => {
         let clickedNote = detectTheNote(e.target, e.target.parentNode, e.target.classList); 
-        if (clickedNote === undefined) {return};
-        let page = clickedNote.parentNode;
-        manageClicks(clickedNote, clickedNote.classList, page);
+        let clickedIcon = detectTheIcon(e.target.id, e.target);
+        if (clickedNote != undefined) {
+            manageNoteClick(clickedNote, clickedNote.classList, clickedNote.parentNode);
+        } else if (clickedIcon != undefined) {
+            manageIconClick(clickedIcon)
+        }
     }))
-
-    function manageClicks(note, classOfNote, page) {
-        if (theNoteIsSaved === true) {
-            const filteredSavedNote = determineNoteColor(savedNote, page.id); // when switching pages, changes the color
-            page.insertBefore(filteredSavedNote, note); // insert the copy before the clicked note
-            stopAnimation();
-        } else if (classOfNote == "new") {
-            createNewNote(page);
-        } else if (note) {
-            savedNote = saveNote(note);
-            startSaveAnimation();
-        } 
-    }
-
-    function detectTheNote(clickedItem, parent, classOfClickedItem) {
-        if (classOfClickedItem == "note" || classOfClickedItem == "new") {
-            return clickedItem;
-        } else if (parent.classList == "txt-container") {
-            return clickedItem.parentNode.parentNode;
-        } else if (parent.id == "deadline") {
-            return clickedItem.parentNode.parentNode.parentNode;
-        }
-    }
-
-    function createNewNote(page) {
-        let dom = getCardDom(page)
-        const coloredNote = determineNoteColor(dom.note, page.id);
-        page.insertBefore(coloredNote, dom.btn); // insert new note before the "Create new note" button
-        addTxtIfFocusOutOrEnter(dom.input);
-    }
-
-    function saveNote(note) {
-        let copy = note.cloneNode(true);
-        note.remove();
-        return copy;
-    }
-
-    function startSaveAnimation() {
-        theNoteIsSaved = true;
-        root.style.setProperty('--hover', 'rgb(137, 180, 250)');
-    }
-
-    function stopAnimation() {
-        theNoteIsSaved = false; // reset
-        root.style.setProperty('--hover', 'none');
-    }
-
-    function determineNoteColor(copy, page) {
-        let coloredNote = copy;
-        switch (page) {
-            case "container-green":  coloredNote.id = "green"; break;
-            case "container-red":    coloredNote.id = "red"; break;
-            case "container-yellow": coloredNote.id = "yellow"; break;
-            case "container-blue":   coloredNote.id = "blue"; break;
-            // css will change the note color based on the Id
-        }
-        return coloredNote;
-    }
-
-    function addTxtIfFocusOutOrEnter(input) {
-        input.addEventListener("blur", () => addTxt(input));
-        input.addEventListener("keydown", (e) => {if(e.key == "Enter"){addTxt(input)}});
-    }
-
-    function addTxt(input) {
-        input.parentNode.textContent = `${input.value}`;
-    }
 })()
-                    
 
-// class GetTags {
-//     constructor(subject, deadline) {
-//         this.subject = subject;
-//         this.deadline = deadline;
-//     }
-// }
+function manageNoteClick(note, classOfNote, page) {
+    if (theNoteIsSaved === true) {
+        const filteredSavedNote = determineNoteColor(savedNote, page.id); // when switching pages, changes the color
+        page.insertBefore(filteredSavedNote, note); // insert the copy before the clicked note
+        stopAnimation();
+    } else if (classOfNote == "new") {
+        createNewNote(page);
+    } else if (note) {
+        savedNote = saveNote(note);
+        startSaveAnimation();
+    } 
+}
 
-// let tag = new GetTags("mathematics", "in a few days")
-// console.log(tag);
+function manageIconClick(icon) {
+    if (icon.id == "doneIcon") {
+        deleteNote(icon.parentNode);
+    } else if (icon.id == "options") {
+        addPopOver();
+    } 
+}
+
+function detectTheNote(clickedItem, parent, classOfClickedItem) {
+    if (classOfClickedItem == "note" || classOfClickedItem == "new") {
+        return clickedItem;
+    } else if (classOfClickedItem == "header-txt") {
+        return clickedItem.parentNode; // return the note
+    } else if (parent.classList == "txt-container") {
+        return clickedItem.parentNode.parentNode; // return the note
+    } else if (parent.id == "deadline") {
+        return clickedItem.parentNode.parentNode.parentNode; // return the note
+    }
+}
+
+function detectTheIcon(id, icon) {
+    if (id == "doneIcon" || id == "options") {
+        return icon;
+    }
+}
+
+function createNewNote(page) {
+    let dom = getCardDom(page)
+    const coloredNote = determineNoteColor(dom.note, page.id);
+    page.insertBefore(coloredNote, dom.btn); // insert new note before the "Create new note" button
+    addTxtIfFocusOutOrEnter(dom.input);
+}
+
+function saveNote(note) {
+    let copy = note.cloneNode(true);
+    deleteNote(note);
+    return copy;
+}
+
+function startSaveAnimation() {
+    theNoteIsSaved = true;
+    root.style.setProperty('--hover', 'rgb(137, 180, 250)');
+}
+
+function stopAnimation() {
+    theNoteIsSaved = false; // reset
+    root.style.setProperty('--hover', 'none');
+}
+
+function determineNoteColor(copy, page) {
+    let coloredNote = copy;
+    switch (page) {
+        case "container-green":  coloredNote.id = "green"; break;
+        case "container-red":    coloredNote.id = "red"; break;
+        case "container-yellow": coloredNote.id = "yellow"; break;
+        case "container-blue":   coloredNote.id = "blue"; break;
+        // css will change the note color based on the Id
+    }
+    return coloredNote;
+}
+
+function addTxtIfFocusOutOrEnter(input) {
+    input.addEventListener("blur", () => addTxt(input));
+    input.addEventListener("keydown", (e) => {if(e.key == "Enter"){addTxt(input)}});
+}
+
+function addTxt(input) {
+    input.parentNode.textContent = `${input.value}`;
+}
+
+function deleteNote(note) {
+    note.remove();
+}
